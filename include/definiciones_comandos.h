b50d6f06122c5cda19f34fb633854867e5df8ec6
Ayelen <ayelenvegacaro@gmail.com>
2024-08-21 19:29:05 -0300

    cambios

--- include/definiciones_comandos.h
+++ include/definiciones_comandos.h
@@ -30,12 +30,35 @@ typedef struct ComandoTemp{
 /**
  * @brief inicializa ComandoStemp  
  * 
- * @param self 
- * @param tempDeseada 
+ * @param self puntero a struct ComandoStemp
+ * @param tempDeseada puntero a VariableInt para temperatura deseada
  */
 void ComandoStemp_init(ComandoStemp *self, VariableInt *tempDeseada);
+
+/**
+ * @brief  inicializa ComandoSpos
+ * 
+ * @param self puntero a struct ComandoSPos
+ * @param posDeseada puntero a VariablePos 
+ */
 void ComandoSpos_init(ComandoSpos *self, VariablePos *posDeseada);
+
+/**
+ * @brief inicializa Pos
+ * 
+ * @param self puntero a struct ComandoPos
+ * @param fin_carrera_dentro puntero a entrada digital de fin carrera dentro
+ * @param fin_carrera_fuera puntero a entrada digital fin de carrera fuera
+ */
 void ComandoPos_init(ComandoPos *self, EntradaDigital *fin_carrera_dentro, EntradaDigital *fin_carrera_fuera);
+
+/**
+ * @brief  inicializa Temp
+ * 
+ * 
+ * @param self puntero a struct Temp
+ * @param termometro puntero a termometro
+ */
 void ComandoTemp_init(ComandoTemp *self, Termometro *termometro);
 
 #endif
